2c9804e9b9bcd6a011bcef33c121c14bd098ccb7
adelhamed2020 <adelh5628@gmail.com>
2020-09-21 12:46:39 +0300

    add comment

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-include './database/database_configuration.php';
+include './database/database_configuration.php'; #hvhjbhjbj
 ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <title></title>
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
